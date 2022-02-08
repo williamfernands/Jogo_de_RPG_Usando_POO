f0e78ab360d7ffe39287a9fa4e80169db48371d1
William Fernandes <68706541+williamfernands@users.noreply.github.com>
2022-02-07 23:41:02 -0300

    criando metodo construtor

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿namespace jogo_rpg_poo
+﻿using Jogo_de_RPG_Usando_POO.SRC.Entities;
+
+namespace jogo_rpg_poo
 {
     class Program
     {
          static void Main(string[] args)
          {
-             
+             Arus hero = new Arus();
+             Console.Write("");
          }
     }
 }
--- SRC/Entities/Arus.cs
+++ SRC/Entities/Arus.cs
@@ -2,6 +2,17 @@ namespace Jogo_de_RPG_Usando_POO.SRC.Entities
 {
     public class Arus
     {
-        
+        public Arus(string Name, int Level, string HeroType){
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+
+        }
+        public string Name;
+
+        public int Level;
+
+        public string HeroType;
+
     }
 }
